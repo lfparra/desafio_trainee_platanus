e08583130d17acd0723cf7d3e9530b188ba58bd4
Leonardo Parra <leoparra95@gmail.com>
2021-03-25 17:48:22 -0300

    Desafío conexión a API Buda

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+require 'json'
+require 'rest-client'
+
+class ArticlesController < ApplicationController
+
+  def index
+
+    url_markets = 'https://www.buda.com/api/v2/markets'
+
+    # Request mercados BUDA
+    
+    response_request_markets = RestClient::Request.execute(method: :get, url: url_markets)
+    result_request_markets = JSON.parse response_request_markets.to_str
+    markets = result_request_markets['markets']
+    
+    # Loop para identificar id/nombre del mercado
+    
+    markets_names = []
+    markets.each do |markets|
+        markets_names.push(markets['id'])
+    end
+    #puts markets_name
+    
+    dict_markets_status = []
+    count = 0
+    # Loop para generar url para get status de cada mercado
+    markets_names.each do |market_name|
+      response_request_market_status = RestClient::Request.execute(method: :get, url: "https://www.buda.com/api/v2/markets/#{market_name}/ticker")
+      
+      #puts response_request_market_status.code
+      
+      result_request_market_status = JSON.parse response_request_market_status.to_str
+      
+      # Acceso a mejores oferta de compra y venta para cada mercado
+      max_bid_market = result_request_market_status['ticker']['max_bid']
+      min_ask_market = result_request_market_status['ticker']['min_ask']
+      
+      # Diccionario con mercado y mejores ofertas últimas 24 horas
+      
+      dict_markets_status[count] = {'id' => count, 
+          'market_name' => market_name, 
+          'max_bid' => {'amount' => max_bid_market[0], 
+          'currency' => max_bid_market[1]}, 
+          'min_ask' => {'amount' => min_ask_market[0], 
+          'currency' => min_ask_market[1]}
+        }
+      count = count + 1
+    end
+    
+    @markets = dict_markets_status
+
+    @time = Time.now
+  
+  end
+end
